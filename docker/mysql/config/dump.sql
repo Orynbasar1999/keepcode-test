274ad0486f5c80bd9a5645336b1205e406a17c0d
Orynbasar1999 <orynbasar1999@gmail.com>
2023-08-24 21:51:13 +0600

    Добавляет докер образы для локального тестирования

---
+++ docker/mysql/config/dump.sql
@@ -0,0 +1,11 @@
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET GLOBAL foreign_key_checks = 0;
+SET SESSION foreign_key_checks = 0;
+SET GLOBAL sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+SET SESSION sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+DROP DATABASE IF EXISTS `keepcode`;
+CREATE DATABASE `keepcode`;
+USE `keepcode`;
+
